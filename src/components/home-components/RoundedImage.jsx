47b23f3fb062c07938674e7f9daf29226ddc06f8
Martyna Kiljan <kmartynak@icloud.com>
2024-08-18 21:26:21 +0200

    test2

--- src/components/home-components/RoundedImage.jsx
+++ src/components/home-components/RoundedImage.jsx
@@ -3,24 +3,33 @@ import { useEffect, useState, useRef } from 'react'
 const RoundedImage = ({ src }) => {
 	const [isInView, setIsInView] = useState(false)
 	const imgRef = useRef(null)
+	const timeoutRef = useRef(null)
 
 	useEffect(() => {
 		const node = imgRef.current
 		if (!node) return
 
 		const handleIntersection = ([entry]) => {
-			requestAnimationFrame(() => {
-				setIsInView(entry.isIntersecting)
-			})
+			if (timeoutRef.current) {
+				clearTimeout(timeoutRef.current)
+			}
+			timeoutRef.current = setTimeout(() => {
+				requestAnimationFrame(() => {
+					setIsInView(entry.isIntersecting)
+				})
+			}, 100) // Opóźnienie 100 ms
 		}
 
 		const observer = new IntersectionObserver(handleIntersection, {
-			threshold: 0.5, // Zwiększ threshold, aby poprawić dokładność wykrywania
+			threshold: 0.5, // Możesz dostosować wartość threshold
 		})
 
 		observer.observe(node)
 
 		return () => {
+			if (timeoutRef.current) {
+				clearTimeout(timeoutRef.current)
+			}
 			observer.unobserve(node)
 		}
 	}, [])
@@ -29,8 +38,9 @@ const RoundedImage = ({ src }) => {
 		<img
 			src={src}
 			ref={imgRef}
+			loading='lazy'
 			className={`rounded-image ${isInView ? 'in-view' : ''}`}
-			style={{ transition: 'transform 0.3s ease-in-out' }} // Dodanie stylów CSS do płynniejszej animacji
+			style={{ transition: 'transform 0.3s ease-in-out' }} // Styl animacji
 		/>
 	)
 }
